355a029f6649287b002a31ca1a9a6256bd8a1893
Victor <victor.garcia.torres1@gmail.com>
2019-12-21 21:35:16 +0100

    Initial Commit

---
+++ iFoodie_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iFoodie_app
+//
+//  Created by Victor Garcia Torres on 21/12/2019.
+//  Copyright © 2019 iFoodieTeam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
